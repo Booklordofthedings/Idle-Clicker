e868b163255381b8da7c86c3c38121c037388f7c
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-08-30 00:02:26 +0200

    game loop

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const Sprites = ["assets/bug.png"]
+const Game = document.getElementById("Game");
+var Difficulty = 0;
+
 //Disables every screen except the one passed in
 function TryEnableScreen(toEnable) {
     document.getElementById("Game").style.display = "none";
@@ -10,18 +14,6 @@ function TryEnableScreen(toEnable) {
 function randomIntFromInterval(min, max) {
     var num = Math.floor(Math.random() * (max - min + 1) + min);
     return num;
-  }
-
-//Create a enemy html object with random settings
-function makeEnemy() {
-    var game = document.getElementById("Game")
-
-    var enemy = new Image(32,32);
-    enemy.src = "assets/bug.png";
-    enemy.style.position = "absolute";
-    enemy.style.top = `${randomIntFromInterval(8, game.offsetHeight - 40)}px`;
-    enemy.style.left = `${randomIntFromInterval(8, game.offsetWidth - 40)}px`;
-    return enemy;
 }
 
 //spawn a new enemy
@@ -30,10 +22,47 @@ async function SpawnEnemy() {
     document.getElementById("Game").appendChild(makeEnemy());
 }
 
+//Creates an enemy object
+function makeEnemy(difficulty, sprites) {
+    if(difficulty === null  || difficulty === undefined)
+        difficulty = 1; //So we dont need to error check
+
+    var enemy = {
+        health: 1 + difficulty,
+        x: randomIntFromInterval(8, Game.offsetWidth - 40),
+        y: randomIntFromInterval(8, Game.offsetHeight - 40),
+        r: randomIntFromInterval(0, 360),
+        draw: function(parent) {
+            var img = new Image(32,32);
+            img.src = sprites[this.health > sprites.lenght ? sprites.lenght : this.health];
+            img.style.position = "absolute";
+            img.style.top = `${this.y}px`;
+            img.style.left = `${this.x}px`;
+            img.style.transform = `rotate(${this.r}deg)`;
+            parent.appendChild(img);
+        }
+    };
+
+    return enemy;
+}
+
 //The game loop
-async function runGame()
+async function runGame(sprites)
 {
     //Retrieve data from storage
+    //Set data if available, otherwise create new data
+    //Retrive strenght data
+    //Spawn new enemies
+    //display new score
+    //save data every x seconds
+
+    //Loop and create a new enemy every x seconds
+    const run = true;
+    while(run) {
+        await new Promise(r => setTimeout(r, 2000)); //Delay TODO: Retrive delay from store
+        var e = makeEnemy(1,Sprites);
+    }
+
 }
 
 
